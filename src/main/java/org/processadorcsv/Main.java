264368e4e04cd00143d0b2112230ec05ad5ad4ec
Roger Bernar <roger.bernar9@gmail.com>
2025-05-22 11:27:03 -0300

    Imprime no console versao java e caminho classpath

--- src/main/java/org/processadorcsv/Main.java
+++ src/main/java/org/processadorcsv/Main.java
@@ -8,6 +8,18 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        String javaVersion = System.getProperty("java.version");
+        String javaHome = System.getProperty("java.home");
+        // Imprime no console versão java e caminho jdk
+        System.out.println("Executando com Java versão: " + javaVersion);
+        System.out.println("Java home (JDK/JRE): " + javaHome);
+        try (java.io.PrintWriter out = new java.io.PrintWriter("versao_java.log")) {
+            out.println("Executando com Java versão: " + javaVersion);
+            out.println("Java home (JDK/JRE): " + javaHome);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         SwingUtilities.invokeLater(() -> new CsvReader().setVisible(true));
     }
 
